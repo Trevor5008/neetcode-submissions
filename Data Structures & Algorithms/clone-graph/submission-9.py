e45bb58d6d116b39b8c49094d538295c008fc311
Trevor C Werner <trezrevner@icloud.com>
2026-08-30 20:56:14 -0400

    Add: clone-graph - submission-9

---
+++ Data Structures & Algorithms/clone-graph/submission-9.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        clone = {}
+
+        def dfs(node):
+            if not node: return
+            if node in clone:
+                return clone[node]
+            clone[node] = Node(node.val)
+            for n in node.neighbors:
+                clone[node].neighbors.append(dfs(n))
+            return clone[node]
+        
+        dfs(node)
+        return clone[node] if node else None
