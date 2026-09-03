6fe9e4b4e27885ee8deabbd78f695c7760df0a5c
Trevor C Werner <trezrevner@icloud.com>
2026-09-03 15:36:57 -0400

    Add: continuous-subarray-sum - submission-10

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-10.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        remainder_map = {0: -1} # {remainder: idx}
+        currSum = 0
+        for i in range(len(nums)):
+            currSum += nums[i]
+            remainder = currSum % k
+            if remainder in remainder_map:
+                print(remainder_map)
+                # check whether the subarray meets length req't
+                if i - remainder_map[remainder] >= 2:
+                    return True
+            else:
+                remainder_map[remainder] = i
+        return False
