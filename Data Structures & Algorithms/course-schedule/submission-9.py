101256dbeb3dd4c9bdfffc3abcc607334b5dbace
Trevor C Werner <trezrevner@icloud.com>
2026-09-13 13:55:26 -0400

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for i in range(numCourses)]
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+
+        courses = [0 for i in range(numCourses)]
+
+        def dfs(course): # cycle detection
+            courses[course] = 1 # visited
+
+            for pre in adj[course]:
+                if courses[pre] == 1:
+                    return True
+                elif courses[pre] == 0:
+                    if dfs(pre):
+                        return True
+            courses[course] = 2 # processed
+            return False
+
+        for i in range(len(courses)):
+            if courses[i] == 1:
+                return False
+            elif courses[i] == 0:
+                if dfs(i):
+                    return False
+
+        return True 
